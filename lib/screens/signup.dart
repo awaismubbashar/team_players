1eee9e1ce51df75a1242ca25ae6ad65fc8a7e7fa
Awais Mubbashar <awaismubbashar@gmail.com>
2024-09-14 19:55:11 +0500

    Added validation on signup screen

--- lib/screens/signup.dart
+++ lib/screens/signup.dart
@@ -8,6 +8,14 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+  final formKey = GlobalKey<FormState>();
+  var passwordController = TextEditingController();
+  var emailController = TextEditingController();
+  var nameController = TextEditingController();
+  var confirmPasswordController = TextEditingController();
+  bool passwordToggle = true;
+  bool confirmPasswordToggle = true;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -16,24 +24,27 @@ class _SignupState extends State<Signup> {
           child: Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  loginText(),
-                  loginDescription(),
-                  const SizedBox(height: 50),
-                  nameTextFormField(),
-                  const SizedBox(height: 20),
-                  emailTextFormField(),
-                  const SizedBox(height: 20),
-                  passwordTextFormField(),
-                  const SizedBox(height: 20),
-                  confirmPasswordTextFormField(),
-                  const SizedBox(height: 30),
-                  loginButton(),
-                  const SizedBox(height: 10),
-                  alreadyAccountText()
-                ],
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    loginText(),
+                    loginDescription(),
+                    const SizedBox(height: 50),
+                    nameTextFormField(),
+                    const SizedBox(height: 20),
+                    emailTextFormField(),
+                    const SizedBox(height: 20),
+                    passwordTextFormField(),
+                    const SizedBox(height: 20),
+                    confirmPasswordTextFormField(),
+                    const SizedBox(height: 30),
+                    loginButton(),
+                    const SizedBox(height: 10),
+                    alreadyAccountText()
+                  ],
+                ),
               ),
             ),
           ),
@@ -52,70 +63,61 @@ class _SignupState extends State<Signup> {
 
   TextFormField nameTextFormField() {
     return TextFormField(
-      decoration: const InputDecoration(
-          focusedBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.blue, width: 1.0),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.grey, width: 1.0),
-          ),
-          prefixIcon: Icon(Icons.account_box_rounded),
-          labelText: 'Name'),
+      controller: nameController,
+      validator: (value) {
+        if(value!.isEmpty) {
+          return 'Name is required';
+        } else if(value.length < 5) {
+          return 'Name have at least 5 characters';
+        }
+      },
+      decoration: fieldDecoration('Name', Icons.account_box_rounded),
     );
   }
 
   TextFormField emailTextFormField() {
     return TextFormField(
-      decoration: const InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.blue, width: 1.0),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-        ),
-        prefixIcon: Icon(Icons.email),
-        labelText: 'Email',
-      ),
+      controller: emailController,
+      validator: emailValidation,
+      decoration: fieldDecoration('Email', Icons.email),
     );
   }
 
   TextFormField passwordTextFormField() {
     return TextFormField(
-      decoration: const InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.blue, width: 1.0),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-        ),
-        suffixIcon: Icon(Icons.remove_red_eye),
-        prefixIcon: Icon(Icons.key),
-        labelText: 'Password',
-      ),
-      obscureText: true,
+      controller: passwordController,
+      validator: passwordValidation,
+      decoration: passwordDecoration('Password'),
+      obscureText: passwordToggle,
     );
   }
 
   TextFormField confirmPasswordTextFormField() {
     return TextFormField(
-      decoration: const InputDecoration(
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.blue, width: 1.0),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.grey, width: 1.0),
-        ),
-        suffixIcon: Icon(Icons.remove_red_eye),
-        prefixIcon: Icon(Icons.key),
-        labelText: 'Confirm Password',
-      ),
-      obscureText: true,
+      controller: confirmPasswordController,
+      validator: (value) {
+        if(value != passwordController.text) {
+          return 'Password does not match' ;
+        } else {
+          return null;
+        }
+      },
+      decoration: passwordDecoration('Confirm Password'),
+      obscureText: confirmPasswordToggle,
     );
   }
 
   ElevatedButton loginButton() {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: () {
+        if (formKey.currentState!.validate()) {
+
+          print("Validated");
+        } else {
+          print(emailController.text);
+          print("Not Validated");
+        }
+      },
       style: ElevatedButton.styleFrom(
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(6), // <-- Radius
@@ -146,5 +148,83 @@ class _SignupState extends State<Signup> {
         ),
       ],
     );
+  }
+
+  // This should move into extension fun
+  InputDecoration fieldDecoration(String name, IconData icon) {
+    return InputDecoration(
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue, width: 1.0),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+        prefixIcon: Icon(icon),
+        labelText: name);
+  }
+
+  String? emailValidation(String? value) {
+    bool emailReg = RegExp(
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+        .hasMatch(value!);
+    if (value.isEmpty) {
+      return "Enter email";
+    } else if (!emailReg) {
+      return "Enter valID email";
+    }
+    return null;
+  }
+
+  InputDecoration passwordDecoration(String name) {
+    return InputDecoration(
+        focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue, width: 1.0),
+        ),
+        enabledBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.grey, width: 1.0),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.red, width: 1.0),
+        ),
+        prefixIcon: const Icon(Icons.key),
+        labelText: name,
+        suffixIcon: InkWell(
+          onTap: () {
+            setState(() {
+              if (name == 'Password') {
+                passwordToggle = !passwordToggle;
+              } else {
+                confirmPasswordToggle = !confirmPasswordToggle;
+              }
+            });
+          },
+          child: Icon((name == 'Password'
+              ? passwordToggle
+                  ? Icons.visibility
+                  : Icons.visibility_off
+              : confirmPasswordToggle
+                  ? Icons.visibility
+                  : Icons.visibility_off)),
+        ));
+  }
+
+  String? passwordValidation(String? value) {
+    if (value!.isEmpty) {
+      return "Enter Password";
+    } else if (value.length < 6) {
+      return "Password should be atleast 6 characters";
+    } else if (value.length > 15) {
+      return "Password should not be greater than 15 characters";
+    }
+    return null;
   }
 }
